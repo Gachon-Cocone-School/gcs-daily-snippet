21c685c1b3bfd00077b28a65f4a69d9bcc287eee
Namjoo Kim <namjookim@gachon.ac.kr>
2025-05-07 16:00:38 +0900

    fix image CORS issue

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    domains: ['lh3.googleusercontent.com'], // Google 프로필 이미지 도메인 추가
+  },
+};
 
 export default config;
